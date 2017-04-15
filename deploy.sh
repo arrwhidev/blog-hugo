dfdc6d353896615742702f2ca2dc0150b99ac82c
arran <arrwhidev@gmail.com>
2017-04-15 16:48:22 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Build content with hugo.
+/home/arran/tools/hugo_0.19_linux_amd64/hugo_0.19_linux_amd64
+
+# Add changes
+cd public
+git add -A
+
+# Default message if not specified.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+
+git commit -m "$msg"
+git push origin master
+cd ..
+
+echo "Deployed!"
